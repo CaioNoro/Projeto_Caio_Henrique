888bfa7706d6a016fab37ee2bb7fbfa9cb15f8b0
Caio Noronha <108815161+CaioNoro@users.noreply.github.com>
2022-12-04 17:01:28 -0300

    Create A.java

---
+++ src/A.java
@@ -0,0 +1,34 @@
+Public class A{
+  
+  private int A1;
+  private float A2;
+  
+  public A(){
+  
+  }
+  
+  public void setA1(int A1){
+    this.A1 = A1;
+  }  
+
+  public void setA2(float A2){
+    this.A2 = A2;
+  }
+  
+  public void getA1(){
+    return A1;
+  }
+  
+  public void getA2(){
+    return A2;
+  }
+  
+  public void MA1(){
+  
+  }
+
+  public void MA2(){
+  
+  }
+
+}
