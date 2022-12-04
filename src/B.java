7dd2449f1e76d10bc613c4fc4ceafb1989db92fb
Caio Noronha <108815161+CaioNoro@users.noreply.github.com>
2022-12-04 17:02:19 -0300

    Create B.java

---
+++ src/B.java
@@ -0,0 +1,34 @@
+Public class B{
+  
+  private int B1;
+  private float B2;
+  
+  public B(){
+  
+  }
+  
+  public void setB1(int B1){
+    this.B1 = B1;
+  }  
+
+  public void setB2(float B2){
+    this.B2 = B2;
+  }
+  
+  public void getB1(){
+    return B1;
+  }
+  
+  public void getB2(){
+    return B2;
+  }
+  
+  public void MB1(){
+  
+  }
+
+  public void MB2(){
+  
+  }
+
+}
