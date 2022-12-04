56b8bc78733e124c0ebdd1089e933054922c8738
CaioNoro <caio.noronha@estudante.ufla.br>
2022-12-04 17:28:06 -0300

    Adicionado metodo MD4

--- src/D.java
+++ src/D.java
@@ -11,7 +11,7 @@ public class D {
         System.out.println("Assinatura método MD2");
     }
 
-    public void MD3(){
-        System.out.println("Método MD3");
+    public void MD4(){
+        System.out.println("Método MD4");
     }
 }
